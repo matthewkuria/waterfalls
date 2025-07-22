df90c993b23d5f261838c51bc8bdcd8672096de9
Matthewkuria <mathewkwachira@gmail.com>
2025-07-22 12:35:44 +0300

    Add the toggle functionality

--- app/components/AccordionLinks.tsx
+++ app/components/AccordionLinks.tsx
@@ -6,7 +6,7 @@ const AccordionLinks = () => {
       <section className="flex flex-col items-center justify-center w-full  mx-auto my-8 md:flex-row md:gap-20 md:justify-between">
           <div className="w-full text-left my-8">
               <p className="p-4 md:p-10  text-gray-700">
-                <span className="uppercase text-xl font-semibold text-secondary">Thika Greens Phase 1,</span> also known as Waterfalls Country Homes, is a premier gated community offering a homogenous lifestyle estate. The estate is 5 minutes drive from The Blue Posts Hotel, off the well-maintained Kandara Road. This upmarket address comprises of 960 quarter acre properties in a secure serviced environment. The infrastructure, comprising of water, electricity, roads and security features is already set up
+                <span className="uppercase text-xl font-semibold text-secondary">Thika Greens Phase 1</span>, also known as Waterfalls Country Homes, is a premier gated community offering a homogenous lifestyle estate. The estate is 5 minutes drive from The Blue Posts Hotel, off the well-maintained Kandara Road. This upmarket address comprises of 960 quarter acre properties in a secure serviced environment. The infrastructure, comprising of water, electricity, roads and security features is already set up.
               </p>
           </div>
           <div className="w-full mx-auto md:my-8 bg-secondary p-2  rounded-lg">
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,67 +1,90 @@
+"use client"
 import Link from 'next/link'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  // Function to handle the toggle of the navigation menu
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="navbar bg-base-100 shadow-sm max-w-screen-xl mx-auto">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+  <div className="navbar bg-base-100 shadow-sm max-w-screen-xl mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div className="btn btn-ghost btn-circle" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ?
+             
+              (
+                <svg className="swap-on fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 512 512">
+            <polygon
+              points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+            </svg>
+              ) :
+               (
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+              )
+              
+            }
+            
+        </div>
+        {/* Dropdown menu */}
+        {isOpen && (
+          <ul
+            className="absolute bg-base-100  z-10 mt-6 w-[300px] h-[30vh] p-2 gap-10 shadow">
+            <li className='w-full hover:bg-base-300 my-2'><Link onClick={toggleMenu} className=" uppercase hover:text-secondary" href="/">Home</Link></li>
+            <li className='w-full hover:bg-base-300 my-2'><Link onClick={toggleMenu} className=" uppercase hover:text-secondary" href="/about-us">About Us</Link></li>
+            <li className='w-full hover:bg-base-300 my-2'><Link onClick={toggleMenu} className=" uppercase hover:text-secondary" href="/amenities">Amenities</Link></li>
+            <li className='w-full hover:bg-base-300 my-2'><Link onClick={toggleMenu} className=" uppercase hover:text-secondary" href="/gallery">Gallery</Link></li>
+            <li className='w-full hover:bg-base-300 my-2'><Link onClick={toggleMenu} className=" uppercase hover:text-secondary" href="/contact-us">Contact Us</Link></li>
+          </ul>
+        )}
       </div>
-      <ul
-        tabIndex={0}
-        className="dropdown-content bg-base-100  z-10 mt-6 w-[300px] h-[30vh] p-2 gap-10 shadow">
-        <li className='w-full hover:bg-base-300'><Link className=" hover:text-secondary" href="/">Home</Link></li>
-        <li className='w-full hover:bg-base-300'><Link className=" hover:text-secondary" href="/about-us">About Us</Link></li>
-        <li className='w-full hover:bg-base-300'><Link className=" hover:text-secondary" href="/amenities">Amenities</Link></li>
-        <li className='w-full hover:bg-base-300'><Link className=" hover:text-secondary" href="/gallery">Gallery</Link></li>        
-        <li className='w-full hover:bg-base-300'><Link className=" hover:text-secondary" href="/contact-us">Contact Us</Link></li>
-      </ul>
-    </div>
-  </div>
-  <div className="navbar-center">
+    </div><div className="navbar-center">
         <Link href="/" className="flex flex-col items-center justify-center text-xs">
           <Image
             src="/images/WATERFALL_LOGO.png"
             alt="Waterfalls Resort Logo"
             width={100}
-            height={40} 
-            className="inline-block mr-2"
-          />
+            height={40}
+            className="inline-block mr-2" />
           <p className="text-secondary hidden"><span className="text-primary mx-1">Waterfalls</span>
             Country Homes Ltd
           </p>
         </Link>
-  </div>
-  <div className="navbar-end">
-    <Link href="/search" >
-    <button className="btn btn-ghost btn-circle">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
-    </button>
-    </Link>
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+      </div><div className="navbar-end">
+        <Link href="/search">
+          <button className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
+          </button>
+        </Link>
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li>
+              <Link href="/login" className="justify-between">
+                Profile
+                <span className="badge">Members</span>
+              </Link>
+            </li>
+            <li><a>Logout</a></li>
+          </ul>
         </div>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <Link href="/login" className="justify-between">
-            Profile
-            <span className="badge">Members</span>
-          </Link>
-        </li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
 </div>
   )
 }
