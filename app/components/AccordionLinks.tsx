f6451c9691a5d206fc4b8a5c40137be507f9d2f3
Matthewkuria <mathewkwachira@gmail.com>
2025-07-22 15:57:26 +0300

    Added the explore more text and svg to  the hero section

--- app/components/AccordionLinks.tsx
+++ app/components/AccordionLinks.tsx
@@ -4,10 +4,16 @@ import React from 'react'
 const AccordionLinks = () => {
   return (
       <section className="flex flex-col items-center justify-center w-full  mx-auto my-8 md:flex-row md:gap-20 md:justify-between">
-          <div className="w-full text-left my-8">
-              <p className="p-4 md:p-10  text-gray-700">
+          <div className="w-full text-left my-8 p-4 md:p-10">
+              <p className="  text-gray-700">
                 <span className="uppercase text-xl font-semibold text-secondary">Thika Greens Phase 1</span>, also known as Waterfalls Country Homes, is a premier gated community offering a homogenous lifestyle estate. The estate is 5 minutes drive from The Blue Posts Hotel, off the well-maintained Kandara Road. This upmarket address comprises of 960 quarter acre properties in a secure serviced environment. The infrastructure, comprising of water, electricity, roads and security features is already set up.
               </p>
+              <Link href="/about-us" className='text-primary hover:font-semibold'>Read More              
+          <span className="">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+            </svg>
+                </span></Link>
           </div>
           <div className="w-full mx-auto md:my-8 bg-secondary p-2  rounded-lg">
               <div className="collapse collapse-plus bg-base-100 border border-base-300">
--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -5,14 +5,26 @@ const HeroSection = () => {
   return (
     <div className="hero flex flex-col w-full h-[85vh]  items-start justify-center md:justify-end">
       <div className="text-background  bg-opacity-50 p-8 md:p-0  w-full md:w-1/2 lg:w-1/3 md:m-5 mt-28 md:mt-0">
-        <h1 className="text-4xl font-bold mb-4">Welcome to <span className="">Waterfalls Country</span><span className="text-secondary px-2">Homes Ltd</span></h1>
-        <p className="mb-8">Your journey to become a house owner starts here.</p>
+        <div className="">
+          <h1 className="text-4xl font-bold mb-4">Welcome to <span className="">Waterfalls Country</span><span className="text-secondary px-2">Homes Ltd</span></h1>
+          <p className="mb-8">Your journey to become a home owner starts here.</p>
+       </div>
         <div className="flex gap-5">
           <Link href="/about-us"><button className="btn btn-primary rounded-md">Explore more</button></Link>
           <Link href="/contact-us">
             <button className="btn bg-transparent text-primary mr-4 hover:bg-background  rounded-md">Contact Us</button>
           </Link>
         </div>
+      </div>
+      <div className="w-full flex justify-center items-center my-10 md:mt-0">
+        <p className="text-primary text-sm font-semibold uppercase mt-4 animate-bounce">
+          Explore more
+          <span className="">
+            <svg xmlns="http://www.w3.org/2000/svg" transform='rotate(90)' className="h-5 w-5 inline-block ml-2" fill="blue" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+            </svg>
+          </span>
+        </p>
       </div>
         
       </div>
